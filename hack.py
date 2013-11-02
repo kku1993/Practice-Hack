27e60af27a9b97acb2b5c74b8c767e99ab1a88ae
kku <kku1993dev@gmail.com>
2013-11-02 16:32:35 -0400

    removed some magic numbers

--- hack.py
+++ hack.py
@@ -1,17 +1,20 @@
 import operator
 from OC import OC
-articles = OC.articles.list(50)
+
+numArticles = 50
+articles = OC.articles.list(numArticles)
 
 titles = dict()
 
 for id, a in articles.items():
   s = a['citation']
-  idx = s.find("accessed on ", 0) + 12
+  idx = s.find("accessed on ", 0) + len("accessed on ")
   date = s[idx:]
 
   titles[a['title']] = (a['views'], date)
 
-titles = sorted(titles.items(), key=operator.itemgetter(1), reverse=True)
+titles = sorted(titles.items(), key=operator.itemgetter(1), reverse=True) #sort by views
 
 for t, data in titles:
+  # data[0] = views, date[1] = date
   print t + ": " + str(data[0]) + "\t " + data[1]
