52ba53c72a6e95bdf77e86565700cbf6e4a83a91
kku <kku1993dev@gmail.com>
2013-11-02 16:23:10 -0400

    added date to list

--- hack.py
+++ hack.py
@@ -5,6 +5,13 @@ articles = OC.articles.list(50)
 titles = dict()
 
 for id, a in articles.items():
-  titles[a['title']] = a['views']
+  s = a['citation']
+  idx = s.find("accessed on ", 0) + 12
+  date = s[idx:]
 
-print sorted(titles.items(), key=operator.itemgetter(1), reverse=True)
+  titles[a['title']] = (a['views'], date)
+
+titles = sorted(titles.items(), key=operator.itemgetter(1), reverse=True)
+
+for t, data in titles:
+  print t + ": " + str(data[0]) + "\t " + data[1]
